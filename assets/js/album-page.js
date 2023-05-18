0178cda54055aaefbeae8f814b48b1f1521665ed
Tommaso Brigaglia <91462387+toretto2002@users.noreply.github.com>
2023-05-18 15:49:19 +0200

    prime prove

--- assets/js/album-page.js
+++ assets/js/album-page.js
@@ -2,6 +2,84 @@ const ALBUM_ENDPOINT =
   "https://striveschool-api.herokuapp.com/api/deezer/album/";
 ("https://striveschool-api.herokuapp.com/api/deezer/album/");
 
+// crea un canvas con l'immagine e ne ritorno il context 2d
+const draw = function (img) {
+  let canvas = document.createElement("canvas");
+  let c = canvas.getContext("2d");
+  c.width = canvas.width = img.clientWidth;
+  c.height = canvas.height = img.clientHeight;
+  c.clearRect(0, 0, c.width, c.height);
+  c.drawImage(img, 0, 0, img.clientWidth, img.clientHeight);
+  return c;
+};
+
+// scompone pixel per pixel e ritorna un oggetto con una mappa della loro frequenza nell'immagine
+const getColors = function (c) {
+  let col,
+    colors = {};
+  let pixels, r, g, b, a;
+  r = g = b = a = 0;
+  pixels = c.getImageData(0, 0, c.width, c.height);
+  for (let i = 0, data = pixels.data; i < data.length; i += 4) {
+    r = data[i];
+    g = data[i + 1];
+    b = data[i + 2];
+    a = data[i + 3];
+    if (a < 255 / 2) continue;
+    col = rgbToHex(r, g, b);
+    if (!colors[col]) colors[col] = 0;
+    colors[col]++;
+  }
+  return colors;
+};
+
+// trova il colore più ricorrente data una mappa di frequenza dei colori
+const findMostRecurrentColor = function (colorMap) {
+  let highestValue = 0;
+  let mostRecurrent = null;
+  for (const hexColor in colorMap) {
+    if (colorMap[hexColor] > highestValue) {
+      mostRecurrent = hexColor;
+      highestValue = colorMap[hexColor];
+    }
+  }
+  return mostRecurrent;
+};
+
+// converte un valore in rgb a un valore esadecimale
+const rgbToHex = function (r, g, b) {
+  if (r > 255 || g > 255 || b > 255) {
+    throw "Invalid color component";
+  } else {
+    return ((r << 16) | (g << 8) | b).toString(16);
+  }
+};
+
+// inserisce degli '0' se necessario davanti al colore in esadecimale per renderlo di 6 caratteri
+const pad = function (hex) {
+  return ("000000" + hex).slice(-6);
+};
+
+window.onload = function () {
+  // prendo il riferimento all'immagine del dom
+  let imgReference = document.querySelector("#img");
+
+  // creo il context 2d dell'immagine selezionata
+  let context = draw(imgReference);
+
+  // creo la mappa dei colori più ricorrenti nell'immagine
+  let allColors = getColors(context);
+
+  // trovo colore più ricorrente in esadecimale
+  let mostRecurrent = findMostRecurrentColor(allColors);
+
+  // se necessario, aggiunge degli '0' per rendere il risultato un valido colore esadecimale
+  let mostRecurrentHex = pad(mostRecurrent);
+
+  // console.log del risultato
+  console.log(mostRecurrentHex);
+};
+
 let addressBarContent = new URLSearchParams(window.location.search);
 let albumId = addressBarContent.get("albumId");
 let albumTitle = document.getElementById("album-title");
@@ -109,6 +187,7 @@ fetch(ALBUM_ENDPOINT + albumId)
     console.log("sono qui", singleAlbum);
     albumTitle.innerHTML = singleAlbum.title;
     albumCover.src = `${singleAlbum.cover_medium}`;
+    albumCover.id = "img";
     artistName.innerHTML = `<a id="pop-title" href="./artist-page.html?artistId=${singleAlbum.artist.id}">${singleAlbum.artist.name}</a>`;
     year.innerHTML = singleAlbum.release_date;
     tracks.innerHTML = singleAlbum.nb_tracks;
@@ -167,4 +246,22 @@ fetch(ALBUM_ENDPOINT + albumId)
 
 window.onload = () => {
   playButton();
+
+  // prendo il riferimento all'immagine del dom
+  let imgReference = document.querySelector("#img");
+
+  // creo il context 2d dell'immagine selezionata
+  let context = draw(imgReference);
+
+  // creo la mappa dei colori più ricorrenti nell'immagine
+  let allColors = getColors(context);
+
+  // trovo colore più ricorrente in esadecimale
+  let mostRecurrent = findMostRecurrentColor(allColors);
+
+  // se necessario, aggiunge degli '0' per rendere il risultato un valido colore esadecimale
+  let mostRecurrentHex = pad(mostRecurrent);
+
+  // console.log del risultato
+  console.log(mostRecurrentHex);
 };
